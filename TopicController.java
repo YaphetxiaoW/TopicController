ef52e5b076d079e4ad58a8e77963d3d8bfaa8ff8
YaphetxiaoW <1219527961@qq.com>
2016-12-31 15:18:28 +0800

    Update TopicController

--- TopicController.java
+++ TopicController.java
@@ -1,5 +1,7 @@
 package cn.edu.bjtu.weibo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,41 +9,72 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import cn.edu.bjtu.weibo.model.Topic;
 import cn.edu.bjtu.weibo.service.HotTopicService;
-//import org.springframework.stereotype.Controller;
 import cn.edu.bjtu.weibo.service.TopicService;
+import cn.edu.bjtu.weibo.model.BaseContent;
+import cn.edu.bjtu.weibo.model.Result;
 
 @RestController
 @RequestMapping("/t")
-public class RegisterController {
+public class TopicController {
 
 	@Autowired
 	TopicService topicService;	
+	
 	@Autowired
 	HotTopicService hotTopicService;
-	@Autowired
+	
 	Result result;
+	
+	
 
 	/**
-	 * 返回前十热门话题
+	 * return the top ten hotTopic
 	 * @return
 	 */
 	@RequestMapping(value = "/hotTopic", method = { RequestMethod.GET, RequestMethod.POST })
 	public @ResponseBody Result getHotTopic() {
-		result.setData(hotTopicService.HotTopic(1, 10));
-       return result;
+		
+		List<Topic> topicList=hotTopicService.HotTopic(1, 10);	
+		Result result = new Result();
+		
+		if(topicList==null||topicList.isEmpty()){
+			result.setStatus(Result.FAILED);
+			result.setTipCode("401");
+			result.setTipMsg("Get hotTopic falied");
+			return result;
+		}
+		
+		result.setStatus(Result.SUCCESS);			
+		result.setTipCode("200");			
+		result.setTipMsg("Get hotTopic success");		
+		result.setData(topicList);       
+		return result;
 	}
 	
 	
 	/**
-	 *  When a user want to see all the weibo or comment contain the topic, this service will help
+	 *  When a user want to see all the weibo or comment contain the topic, this function will help 
 	 * @return
 	 */
 	@RequestMapping(value = "/topic", method = { RequestMethod.GET, RequestMethod.POST })
-	public @ResponseBody Result getAllTopic(@RequestBody  Info info) {
+	public @ResponseBody Result getAllTopic(String topicId,int pageIndex,int pagePerPage) {
 		
-		result.setData(topicService.getWeiboOrCommentListUnderTopic(Info.topicId, Info.pageIndex, Info.pagePerPage));   
+		List<BaseContent> BaseContentList=topicService.getWeiboOrCommentListUnderTopic(topicId, pageIndex,pagePerPage);	
+		Result result = new Result();
+		
+		if(BaseContentList==null||BaseContentList.isEmpty()){
+			result.setStatus(Result.FAILED);
+			result.setTipCode("401");
+			result.setTipMsg("Get BaseContent falied");
+			return result;
+		}
+		
+		result.setStatus(Result.SUCCESS);			
+		result.setTipCode("200");			
+		result.setTipMsg("Get BaseContent success");		
+		result.setData(BaseContentList);   
 		return result;
 	}
 }
